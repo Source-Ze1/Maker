0130bf12f79731ea153cdb4e07014baf95dc06ac
Source-Ze1 <146869174+Source-Ze1@users.noreply.github.com>
2023-10-03 22:49:29 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["UP_UO"]
+OWNER_NAME = "mody"
+BOT_TOKEN = "6583769930:AAGpuaT7jh51g7XBPgsjDdN5UnSVuCwsRtw"
+DATABASE = "mongodb+srv://albolbol:albolbolzpy@cluster0.cpd7rm5.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/UI_XB"
+GROUP = "https://t.me/UI_OS"
+VIDEO = "https://telegra.ph/file/7065a4c7cbedef8befdb9.jpg"
+LOGS = "zemaker"
+
